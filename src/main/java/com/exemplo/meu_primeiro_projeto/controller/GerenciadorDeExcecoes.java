4cfdfe36f0e8d7a979a929f5e676f644c02b3dd6
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-07-03 20:17:28 -0300

    feat: adiciona tratamento de erros global com records e dto

--- src/main/java/com/exemplo/meu_primeiro_projeto/controller/GerenciadorDeExcecoes.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/controller/GerenciadorDeExcecoes.java
@@ -3,7 +3,10 @@ package com.exemplo.meu_primeiro_projeto.controller;
 import java.time.LocalDateTime;
 import java.util.NoSuchElementException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,7 +22,42 @@ public class GerenciadorDeExcecoes {
             "O ID informado não corresponde a nenhum produto no catálogo.",
             LocalDateTime.now()
         );
-        return ResponseEntity.status(404).body(erro);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RespostaErro> tratarErroValidacao(MethodArgumentNotValidException ex) {
+        String mensagem = ex.getBindingResult().getFieldError().getDefaultMessage();
+        
+        var erro = new RespostaErro(
+            "Dados enviados não passam nas regras de validação.",
+            mensagem,
+            LocalDateTime.now()
+        );
+
+        return ResponseEntity.badRequest().body(erro);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<RespostaErro> tratarJsonErro(HttpMessageNotReadableException ex) {
+        var erro = new RespostaErro(
+          "Corpo da requisição inválido (JSON malformado).",
+          "Certifique-se de que o JSON enviado está com a sintaxe correta.",
+          LocalDateTime.now()
+        );
+
+        return ResponseEntity.badRequest().body(erro);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<RespostaErro> tratarErroGenerico (Exception ex) {
+        var erro = new RespostaErro (
+            "Erro interno no servidor",
+            "Ocorreu uma falha inesperada no backend",
+            LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(erro);
     }
 
 }
--- src/main/java/com/exemplo/meu_primeiro_projeto/dto/RespostaErro.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/dto/RespostaErro.java
@@ -1,5 +1,9 @@
 package com.exemplo.meu_primeiro_projeto.dto;
 
-public record RespostaErro() {
+import java.time.LocalDateTime;
 
-}
+public record RespostaErro(
+    String mensagem,
+    String detalhes,
+    LocalDateTime timestamp
+) { }
