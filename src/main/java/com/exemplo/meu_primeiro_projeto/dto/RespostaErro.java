9235733eb165defa51d01bcbbdb74e054200d34f
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-07-08 23:14:35 -0300

    movendo de local

--- src/main/java/com/exemplo/meu_primeiro_projeto/dto/RespostaErro.java
+++
@@ -1,9 +0,0 @@
-package com.exemplo.meu_primeiro_projeto.dto;
-
-import java.time.LocalDateTime;
-
-public record RespostaErro(
-    String mensagem,
-    String detalhes,
-    LocalDateTime timestamp
-) { }
