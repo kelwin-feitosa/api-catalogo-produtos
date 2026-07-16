d21a33f6629fcacef83214333bb5ba1897d5b511
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-07-16 17:16:08 -0300

    feat: implementa regras de negócio para produtos

--- src/main/java/com/exemplo/meu_primeiro_projeto/service/ProdutoService.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/service/ProdutoService.java
@@ -4,87 +4,116 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.exemplo.meu_primeiro_projeto.dto.ProdutoRequisicao;
-import com.exemplo.meu_primeiro_projeto.dto.ProdutoResposta;
+import com.exemplo.meu_primeiro_projeto.dto.ProdutoRequest;
+import com.exemplo.meu_primeiro_projeto.dto.ProdutoResponse;
+import com.exemplo.meu_primeiro_projeto.exception.CategoriaNaoEncontradaException;
+import com.exemplo.meu_primeiro_projeto.exception.ProdutoJaExisteException;
 import com.exemplo.meu_primeiro_projeto.exception.ProdutoNaoEncontradoException;
+import com.exemplo.meu_primeiro_projeto.model.Categoria;
 import com.exemplo.meu_primeiro_projeto.model.Produto;
+import com.exemplo.meu_primeiro_projeto.repository.CategoriaRepository;
 import com.exemplo.meu_primeiro_projeto.repository.ProdutoRepository;
 
 @Service
 public class ProdutoService {
     
     private final ProdutoRepository repository;
+    private final CategoriaRepository categoriaRepository;
     
-    public ProdutoService(ProdutoRepository repository) {
+    public ProdutoService(ProdutoRepository repository, CategoriaRepository categoriaRepository) {
         this.repository = repository;
+        this.categoriaRepository = categoriaRepository;
     }
 
 
-    public List<ProdutoResposta> listarProdutos() {
+    public List<ProdutoResponse> listarProdutos() {
         return repository.findAll()
                 .stream()
                 .map(this::converterParaResponse)
                 .toList();
     }
 
-    public ProdutoResposta criarProduto(ProdutoRequisicao request) {
-        Produto produto = converterParaProduto(request);
+    public ProdutoResponse criarProduto(ProdutoRequest request) {
+        verificarDuplicidade(request);
+        Categoria categoria = buscarCategoria(request.categoriaId());
 
+        Produto produto = converterParaProduto(request, categoria);
         Produto salvo = repository.save(produto);
 
-        return new ProdutoResposta (
-            salvo.getId(),
-            salvo.getNome(),
-            salvo.getPreco(),
-            salvo.getDescricao(),
-            salvo.getQuantidadeEstoque()
-        );
+        return converterParaResponse(salvo);
     }
 
-    public ProdutoResposta atualizarProduto(Long id, ProdutoRequisicao atualizandoProduto) {
-        Produto produto = repository.findById(id)
-                        .orElseThrow(() -> new ProdutoNaoEncontradoException("Produto não encontrado."));
+    public ProdutoResponse atualizarProduto(Long id, ProdutoRequest atualizandoProduto) {
+        Produto produto = buscarEntidade(id);
+        verificarDuplicidade(produto, atualizandoProduto);
         
         produto.setNome(atualizandoProduto.nome());
         produto.setPreco(atualizandoProduto.preco());
         produto.setDescricao(atualizandoProduto.descricao());
         produto.setQuantidadeEstoque(atualizandoProduto.quantidadeEstoque());
+        Categoria categoria = buscarCategoria(atualizandoProduto.categoriaId());
+        produto.setCategoria(categoria);
 
         Produto produtoAtualizado = repository.save(produto);
-
         return converterParaResponse(produtoAtualizado);
     }
 
-    public ProdutoResposta buscarPorId(Long id) {
-        Produto produto = repository.findById(id)
-                            .orElseThrow(() -> new ProdutoNaoEncontradoException("Produto não encontrado."));
-
-        return converterParaResponse(produto);
+    public ProdutoResponse buscarPorId(Long id) { //Para o usuário
+        return converterParaResponse(buscarEntidade(id));
     }
 
     public void deletarProduto(Long id) {
-        Produto produto = repository.findById(id)
-                            .orElseThrow(() -> new ProdutoNaoEncontradoException("Produto não encontrado."));
-
+        Produto produto = buscarEntidade(id);
         repository.delete(produto);                   
     }
 
-    private Produto converterParaProduto(ProdutoRequisicao request) {
-        return new Produto (
+    private Produto converterParaProduto(ProdutoRequest request, Categoria categoria) {
+        return new Produto(
             request.nome(),
             request.preco(),
             request.descricao(),
-            request.quantidadeEstoque()
+            request.quantidadeEstoque(),
+            categoria
         );
     }
 
-    private ProdutoResposta converterParaResponse(Produto produto){
-        return new ProdutoResposta (
+    private ProdutoResponse converterParaResponse(Produto produto){
+        Long categoriaId = produto.getCategoria() != null
+            ? produto.getCategoria().getId()
+            : null;
+
+        return new ProdutoResponse (
             produto.getId(),
             produto.getNome(),
             produto.getPreco(),
             produto.getDescricao(),
-            produto.getQuantidadeEstoque()
+            produto.getQuantidadeEstoque(),
+            categoriaId,
+            produto.getDataCadastro()
         );
     }
+
+    private Produto buscarEntidade(Long id) { //Para uso interno
+        return repository.findById(id)
+            .orElseThrow(() -> new ProdutoNaoEncontradoException("Produto não encontrado."));
+    }
+
+    private Categoria buscarCategoria(Long id) {
+        return categoriaRepository.findById(id)
+                .orElseThrow(() -> 
+                    new CategoriaNaoEncontradaException("Categoria não encontrada.")
+                );
+    }
+    
+    private void verificarDuplicidade(ProdutoRequest request) {
+        if(repository.existsByNome(request.nome())) {
+            throw new ProdutoJaExisteException("Já existe um produto com esse nome.");
+        }
+    }
+
+    private void verificarDuplicidade(Produto produto, ProdutoRequest request) {
+    if(!produto.getNome().equals(request.nome())) {
+        verificarDuplicidade(request);
+    }
+}
 }
