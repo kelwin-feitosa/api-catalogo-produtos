0d8b9b3721d41188b902d2cb8fb1b343617a5294
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-07-16 17:14:45 -0300

    feat: adiciona consulta por nome de produto

--- src/main/java/com/exemplo/meu_primeiro_projeto/repository/ProdutoRepository.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/repository/ProdutoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.exemplo.meu_primeiro_projeto.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    
+    boolean existsByNome(String nome);
 }
