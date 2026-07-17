5124d5299c9b3270ddac7d96633555e8db20042a
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-07-17 17:00:50 -0300

    refactor: melhora relacionamentos da entidade cliente

--- src/main/java/com/exemplo/meu_primeiro_projeto/model/Cliente.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/model/Cliente.java
@@ -1,18 +1,28 @@
 package com.exemplo.meu_primeiro_projeto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 
 @Entity
-public class Cliente { // O cliente
+public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne(mappedBy = "cliente")
+    private Carrinho carrinho;
 
+    @OneToMany(mappedBy = "cliente")
+    private List<Venda> vendas = new ArrayList<>();
+    
     private String nome;
     private String email;
     private String telefone;
@@ -27,6 +37,12 @@ public class Cliente { // O cliente
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
+    public Carrinho getCarrinho() { return carrinho; }
+    public void setCarrinho(Carrinho carrinho) { this.carrinho = carrinho; }
+
+    public List<Venda> getVendas() { return vendas; }
+    public void setVendas(List<Venda> vendas) { this.vendas = vendas; }
 
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
