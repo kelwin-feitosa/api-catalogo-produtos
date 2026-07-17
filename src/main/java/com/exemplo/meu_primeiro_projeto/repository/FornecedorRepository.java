1fb8b0c9be8ed3f834025331f24d49066395fbc6
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-07-17 17:17:10 -0300

    feat: adiciona busca por cnpj no repository de Fornecedor

--- src/main/java/com/exemplo/meu_primeiro_projeto/repository/FornecedorRepository.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/repository/FornecedorRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.exemplo.meu_primeiro_projeto.model.Fornecedor;
 
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
-
+    boolean existsByCnpj(String cnpj);
 }
